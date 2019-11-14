7a4929a37b03a57d45c8fe87d5c0d93f03c25fa6
qianfengg <fred_gqf0221@163.com>
2019-11-14 22:16:23 +0800

    粒子效果扩展 jq的简单封装

--- 10-particle_canvas/particle.js
+++ 10-particle_canvas/particle.js
@@ -14,55 +14,49 @@ const randomColor = () => "#" + (random() * 0xffffff | 0).toString(16).padStart(
 const deg2Rad = deg => deg * PI / 180;
 
 class Particle {
-    constructor({x, y, r, speedX, speedY, speedR}) {
+    constructor({x, y, r, speedX, speedY, speedR, targetR}) {
         this.x = x;
         this.y = y;
         this.r = r;
-        if (!Array.isArray(speedX)) {
-            this.vx = randomRange(speedX, -speedX);
-        } else {
-            this.vx = randomRange(speedX[0], speedX[1]);
-        }
-        if (!Array.isArray(speedY)) {
-            this.vy = randomRange(speedY, -speedY);
-        } else {
-            this.vy = randomRange(speedY[0], speedY[1]);
-        }
-        this.vr = this.targetR = speedR;
         this.color = randomColor();
+        this.vx = randomRange(speedX[0], speedX[1]);
+        this.vy = randomRange(speedY[0], speedY[1]);
+        this.vr = speedR;
+        this.targetR = targetR;
     }
 
-    update() {
+    update(){
         this.x += this.vx;
         this.y += this.vy;
-        this.r -= this.vr;
+        this.r += this.vr;
     }
 }
 
 class ParticleCanvas {
     constructor(options) {
         let defaultOpts = {
-            cW: 300,
-            cH: 150,
-            r: 8,
-            speedX: 4,
-            speedY: 4,
-            speedR: 0.02,
-            type: 0,
+            cW: 300,  //canvas宽度
+            cH: 150, //canvas高度
+            bornX: "center", //x轴出生位置 left 左 center 中间 right 右 random 随机
+            bornY: "middle", //y轴出生位置 top 上 middle 中间 bottom 下 random 随机
+            r: 3, //粒子初始半径
+            speedX: [-4, 4], //粒子x方向速度范围，数组2个元素，速度可以随意换，也可以写成[4, -4]，但必须写2个值且传入的是数组
+            speedY: [-4, 4], //粒子y方向速度范围，数组2个元素，速度可以随意换，也可以写成[4, -4]，但必须写2个值且传入的是数组
+            speedR: -0.02, //半径速度，负数为半径变小， 正数为半径变大
+            targetR: 0.02 //目标半径，当半径一直减小，有个目标点，不能减到负数吧。从数组中删除，当半径增大，也能设置目标点，不能一直变大吧
         };
-        this.opts = {...defaultOpts, ...options};
+        this.opts = Object.assign(defaultOpts, options);
         this.particles = [];
         this.timer = 0;
     }
 
     init() {
         let oCvs = document.createElement("canvas");
+        this.ctx = oCvs.getContext("2d");
         this.cW = oCvs.width = this.opts.cW;
         this.cH = oCvs.height = this.opts.cH;
-        this.ctx = oCvs.getContext("2d");
         document.body.appendChild(oCvs);
         this.drawBg();
-
     }
 
     drawBg() {
@@ -70,18 +64,25 @@ class ParticleCanvas {
         this.ctx.fillRect(0, 0, this.cW, this.cH);
     }
 
-    spawnParticles() {
-        let types = [{x: this.opts.cW / 2, y: this.opts.cH / 2}, {
-            x: randomRange(this.cW),
-            y: randomRange(this.cH)
-        }, {x: this.cW / 2, y: this.cH}];
+    spawnParticle() {
+        let posX = {
+            left: 0,
+            center: this.cW / 2,
+            right: this.cW,
+        };
+        let posY = {
+            top: 0,
+            middle: this.cH / 2,
+            bottom: this.cH
+        };
         this.particles.push(new Particle({
-            x: types[this.opts.type].x,
-            y: types[this.opts.type].y,
+            x: posX[this.opts.bornX] ? posX[this.opts.bornX] : randomRange(this.cW),
+            y: posY[this.opts.bornY] ? posY[this.opts.bornY] : randomRange(this.cH),
             r: this.opts.r,
             speedX: this.opts.speedX,
             speedY: this.opts.speedY,
             speedR: this.opts.speedR,
+            targetR: this.opts.targetR,
         }));
     }
 
@@ -93,14 +94,25 @@ class ParticleCanvas {
             this.ctx.fill();
             item.update();
         });
-        this.particles = this.particles.filter(item => item.r > item.targetR);
+        this.deleteParticles();
+    }
+
+    deleteParticles(){
+        this.particles = this.particles.filter(item => {
+            if(item.vr < 0){
+                //半径减小，粒子越来越小，比目标半径小了就删除
+                return item.r > item.targetR;
+            }else{
+                //半径增大，粒子越来越大，比目标半径大了就删除
+                return item.r < item.targetR;
+            }
+        })
     }
 
     loop() {
-        this.ctx.fillStyle = "rgba(0, 0, 0, .2)";
+        this.ctx.fillStyle = "rgba(0,0,0,.2)";
         this.ctx.fillRect(0, 0, this.cW, this.cH);
-        this.drawBg();
-        this.spawnParticles();
+        this.spawnParticle();
         this.drawParticles();
         this.timer = requestAnimationFrame(this.loop.bind(this));
     }
---
+++ 11-my_JQ/myJQ.js
@@ -0,0 +1,113 @@
+class JQ{
+    constructor(arg, root){
+        if(typeof root === "undefined"){
+            this.prevObject = new JQ(document, null);
+        }
+        if(root){
+            this.prevObject = root;
+        }
+        if(typeof arg === "string"){
+            // console.log("选择器");
+            let eleArr = document.querySelectorAll(arg);
+            this.addProperty(eleArr);
+        }else if(typeof arg === "function"){
+            // console.log("方法");
+            window.addEventListener("DOMContentLoaded", arg);
+        }else{
+            // console.log("原生节点");
+            if (typeof arg.length === "undefined") {
+                //只有一个原生节点
+                this.length = 1;
+                this[0] = arg;
+            }else{
+                //多个原生节点
+                this.addProperty(arg);
+            }
+        }
+    }
+
+    addProperty(obj){
+        this.length = obj.length;
+        for (let i = 0; i < this.length; i++) {
+            this[i] = obj[i];
+        }
+    }
+
+    click(fn){
+        for (let i = 0; i < this.length; i++) {
+            this[i].addEventListener("click", fn);
+        }
+    }
+
+    on(eventType, fn){
+        let eventTypeArr = eventType.split(" ");
+        for (let i = 0; i < eventTypeArr.length; i++) {
+            for (let j = 0; j < this.length; j++) {
+                this[j].addEventListener(eventTypeArr[i], fn);
+            }
+        }
+    }
+
+    eq(index){
+        return new JQ(this[index], this);
+    }
+
+    end(){
+        return this.prevObject;
+    }
+
+    css(...args){
+        if(args.length === 1){
+            if(typeof args[0] === "string"){
+                // console.log("获取属性")
+                return this.getStyle(this[0], args[0]);
+            }else if(typeof args[0] === "object"){
+                // console.log("对象设置属性")
+                for (let i = 0; i < this.length; i++) {
+                    for (let key in args[0]) {
+                        this.setStyle(this[i], key, args[0][key]);
+                    }
+                }
+            }
+        }else if(args.length === 2){
+            // console.log("设置属性")
+            for (let i = 0; i < this.length; i++) {
+                this.setStyle(this[i], args[0], args[1]);
+            }
+        }
+    }
+
+    getStyle(el, attr){
+        return getComputedStyle(el)[attr];
+    }
+
+    static needAddPx = [
+        "width",
+        "height",
+        "marginLeft",
+        "marginTop",
+        "marginRight",
+        "marginBottom",
+        "paddingLeft",
+        "paddingTop",
+        "paddingRight",
+        "paddingBottom",
+        "left",
+        "top",
+        "right",
+        "bottom"
+    ];
+
+    setStyle(el, attr, val){
+        if(JQ.needAddPx.includes(attr)){
+            el.style[attr] = parseFloat(val) + "px";
+        }else{
+            el.style[attr] = val;
+        }
+
+    }
+}
+function $(arg){
+    return new JQ(arg);
+}
+//需要扩展加px的属性可以这样添加 JQ.needAddPx.push("xxxxx")
