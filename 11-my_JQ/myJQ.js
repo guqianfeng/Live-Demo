27cbf7b3bf9f004c0fdbedfcb8dc80519d8777f4
qianfengg <fred_gqf0221@163.com>
2019-11-15 23:07:50 +0800

    简单优化css设置值

--- 11-my_JQ/myJQ.js
+++ 11-my_JQ/myJQ.js
@@ -1,67 +1,69 @@
-class JQ{
-    constructor(arg, root){
-        if(typeof root === "undefined"){
+class JQ {
+    constructor(arg, root) {
+        if (typeof root === "undefined") {
             this.prevObject = new JQ(document, null);
         }
-        if(root){
+        if (root) {
             this.prevObject = root;
         }
-        if(typeof arg === "string"){
+        if (typeof arg === "string") {
             // console.log("选择器");
             let eleArr = document.querySelectorAll(arg);
             this.addProperty(eleArr);
-        }else if(typeof arg === "function"){
+        } else if (typeof arg === "function") {
             // console.log("方法");
             window.addEventListener("DOMContentLoaded", arg);
-        }else{
+        } else {
             // console.log("原生节点");
             if (typeof arg.length === "undefined") {
                 //只有一个原生节点
                 this.length = 1;
                 this[0] = arg;
-            }else{
+            } else {
                 //多个原生节点
                 this.addProperty(arg);
             }
         }
     }
 
-    addProperty(obj){
+    addProperty(obj) {
         this.length = obj.length;
         for (let i = 0; i < this.length; i++) {
             this[i] = obj[i];
         }
     }
 
-    click(fn){
+    click(fn) {
         for (let i = 0; i < this.length; i++) {
             this[i].addEventListener("click", fn);
         }
+        return this;
     }
 
-    on(eventType, fn){
+    on(eventType, fn) {
         let eventTypeArr = eventType.split(" ");
         for (let i = 0; i < eventTypeArr.length; i++) {
             for (let j = 0; j < this.length; j++) {
                 this[j].addEventListener(eventTypeArr[i], fn);
             }
         }
+        return this;
     }
 
-    eq(index){
+    eq(index) {
         return new JQ(this[index], this);
     }
 
-    end(){
+    end() {
         return this.prevObject;
     }
 
-    css(...args){
-        if(args.length === 1){
-            if(typeof args[0] === "string"){
+    css(...args) {
+        if (args.length === 1) {
+            if (typeof args[0] === "string") {
                 // console.log("获取属性")
                 return this.getStyle(this[0], args[0]);
-            }else if(typeof args[0] === "object"){
+            } else if (typeof args[0] === "object") {
                 // console.log("对象设置属性")
                 for (let i = 0; i < this.length; i++) {
                     for (let key in args[0]) {
@@ -69,7 +71,7 @@ class JQ{
                     }
                 }
             }
-        }else if(args.length === 2){
+        } else if (args.length === 2) {
             // console.log("设置属性")
             for (let i = 0; i < this.length; i++) {
                 this.setStyle(this[i], args[0], args[1]);
@@ -78,39 +80,43 @@ class JQ{
         return this;
     }
 
-    getStyle(el, attr){
+    getStyle(el, attr) {
         return getComputedStyle(el)[attr];
     }
 
-    static needAddPx = [
-        "width",
-        "height",
-        "marginLeft",
-        "marginTop",
-        "marginRight",
-        "marginBottom",
-        "paddingLeft",
-        "paddingTop",
-        "paddingRight",
-        "paddingBottom",
-        "left",
-        "top",
-        "right",
-        "bottom"
-    ];
-
-    setStyle(el, attr, val){
-        if(JQ.needAddPx.includes(attr)){
-            el.style[attr] = parseFloat(val) + "px";
+    setStyle(el, attr, val) {
+        if(typeof val === "number" && !(attr in $.cssNumber)){
+            el.style[attr] = val + "px";
         }else{
             el.style[attr] = val;
         }
-
     }
 }
-function $(arg){
+
+function $(arg) {
     return new JQ(arg);
 }
-//需要扩展加px的属性可以这样添加 JQ.needAddPx.push("xxxxx")
-//扩展修改，cssNumber cssHooks
+
+$.cssNumber = {
+    "animationIterationCount": true,
+    "columnCount": true,
+    "fillOpacity": true,
+    "flexGrow": true,
+    "flexShrink": true,
+    "fontWeight": true,
+    "gridArea": true,
+    "gridColumn": true,
+    "gridColumnEnd": true,
+    "gridColumnStart": true,
+    "gridRow": true,
+    "gridRowEnd": true,
+    "gridRowStart": true,
+    "lineHeight": true,
+    "opacity": true,
+    "order": true,
+    "orphans": true,
+    "widows": true,
+    "zIndex": true,
+    "zoom": true
+};
 
